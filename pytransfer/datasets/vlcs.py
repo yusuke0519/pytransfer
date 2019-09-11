44a53e78d892ae8182e4d26b9b324b88e65fd1c2
furuta <furuta0313@gmail.com>
2019-09-11 20:02:29 +0900

    vlcs

---
+++ pytransfer/datasets/vlcs.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import os
+import wget
+from scipy.io import loadmat
+
+import numpy as np
+import torch.utils.data as data
+
+from base import DomainDatasetBase
+
+CONFIG = {}
+CONFIG['VOC2007'] = 'https://drive.google.com/uc?export=download&id=1bwuegzRa3MONhmjlJnf6VjWwK1sEkXER'
+CONFIG['LabelMe'] = 'https://drive.google.com/uc?export=download&id=1kaVCI7o9SW4U8UUmc_M4OCqwtBEsNVFJ'
+CONFIG['Caltech101'] = 'https://drive.google.com/uc?export=download&id=1w4XAUGkipynlb1XTErcJA8HE8AwppLiX'
+CONFIG['SUN09'] = 'https://drive.google.com/uc?export=download&id=19FT_9YSF2_EYudWEjOXPRNQDQUO4uRft'
+CONFIG['out'] = os.path.expanduser('~/.torch/datasets')
+
+
+class _SingleVLCS(data.Dataset):
+    path = os.path.expanduser('~/.torch/datasets/')
+    all_domain_key = ['VOC2007', 'LabelMe', 'Caltech101', 'SUN09']
+    input_shape = (4096,)
+    num_classes = 5
+
+    def __init__(self, domain_key):
+        assert domain_key in self.all_domain_key
+        domain_path = self.path + domain_key + '.mat'
+        if not os.path.exists(domain_path):
+            self.download(domain_path, domain_key)
+        self.domain_key = domain_key
+        data = loadmat(domain_path, squeeze_me=True)['data']
+        self.data = data
+
+    def download(self, domain_path, domain_key):
+        output_dir = os.path.dirname(domain_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        wget.download(CONFIG[domain_key], out=domain_path)
+
+    def __getitem__(self, index):
+        return self.data[index][:self.input_shape[0]], self.data[index][self.input_shape[0]] - 1, self.domain_key
+
+    def __len__(self):
+        return len(self.data)
+
+
+class VLCS(DomainDatasetBase):
+    """ VLCS
+
+    Args:
+      domain_keys: a list of domains
+
+    """
+    SingleDataset = _SingleVLCS
