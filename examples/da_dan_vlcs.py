9daf7116f3c14ec9a9d344238480ecd5ab572fa0
furuta <furuta0313@gmail.com>
2019-09-18 14:35:05 +0900

    refactor

--- examples/da_dan_vlcs.py
+++ examples/da_dan_vlcs.py
@@ -16,35 +16,41 @@ from pytransfer.datasets.base import Subset
 from pytransfer.datasets import VLCS
 from pytransfer.trainer import DALearner
 from vlcs_network import Encoder, Classifier
-from pytransfer.reguralizer.dan import DADANReguralizer, DANReguralizer
+from pytransfer.reguralizer.dan import DANReguralizer
 from exp_utils import da_check_invariance, domain_wise_splits
 from tensorboardX import SummaryWriter
 
 
-def prepare_datasets(source_domain, target_domain, ratio=0.8):
-    source_dataset = VLCS(source_domain)
-    target_dataset = VLCS(target_domain)
+def prepare_datasets(source, target, ratio=0.8):
+    vlcs_dataset = VLCS([source, target])
 
-    train_source_dataset, valid_dataset = domain_wise_splits(source_dataset, ratio)
-    train_target_dataset, test_dataset = domain_wise_splits(target_dataset, ratio)
-    return train_source_dataset, valid_dataset, train_target_dataset, test_dataset
+    train_dataset, valid_test_dataset = domain_wise_splits(vlcs_dataset, ratio)
+    return train_dataset, valid_test_dataset
 
 
 if __name__ == '__main__':
     print("Execute experiment")
     # Parameters
-    source_domain = ['V'] # 'V', 'L', 'C', 'S'
-    target_domain = ['L']
+    source = 'V' # 'V', 'L', 'C', 'S'
+    target = 'L'
     optim = {'lr': 0.001, 'batch_size': 64, 'num_batch': 5000}
     alpha = 1.0
-    #alpha = 0
+    # log
+    tbx = False
 
     print("Load datasets")
-    train_source_dataset, valid_dataset, train_target_dataset, test_dataset = prepare_datasets(source_domain, target_domain, 0.8)
-    data_log = "source (%s) train : %d, valid %d| target (%s) train: %d, test %d" % (source_domain[0], len(train_source_dataset), len(valid_dataset), target_domain[0], len(train_target_dataset), len(test_dataset))
+    train_dataset, valid_test_dataset = prepare_datasets(source, target, 0.8)
+    train_source_size = train_dataset.cummulative_sizes[0]
+    valid_size = valid_test_dataset.cummulative_sizes[0]
+    train_target_size = train_dataset.cummulative_sizes[1] - train_source_size
+    test_size = valid_test_dataset.cummulative_sizes[1] - valid_size
+    data_log = "source (%s) train : %d, valid %d| target (%s) train: %d, test %d" % (source, train_source_size, valid_size, target, train_target_size, test_size)
     print(data_log)
-    valid_loader = data.DataLoader(valid_dataset, batch_size=optim['batch_size'], shuffle=True)
-    test_loader = data.DataLoader(test_dataset, batch_size=optim['batch_size'], shuffle=True)
+    valid_sampler = data.sampler.SubsetRandomSampler([i for i in range(0, valid_size)])
+    test_sampler = data.sampler.SubsetRandomSampler([i for i in range(valid_size, valid_test_dataset.cummulative_sizes[1])])
+    source_sampler = data.sampler.SubsetRandomSampler([i for i in range(0, train_source_size)])
+    valid_loader = data.DataLoader(valid_test_dataset, batch_size=optim['batch_size'], sampler=valid_sampler)
+    test_loader = data.DataLoader(valid_test_dataset, batch_size=optim['batch_size'], sampler=test_sampler)
 
     print("Build model...")
     E = Encoder(VLCS.get('input_shape'))
@@ -63,20 +69,20 @@ if __name__ == '__main__':
         "num_domains": 2, # Source & Target
         "input_shape": E.output_shape(), 'hiddens': [E.output_shape()[1]]}
 
-    reg = DADANReguralizer(learner=learner, discriminator_config=discriminator_config)
-    #reg = DANReguralizer(learner=learner, discriminator_config=discriminator_config)
+    reg = DANReguralizer(learner=learner, discriminator_config=discriminator_config)
     reg_optimizer = RMSprop(filter(lambda p: p.requires_grad, reg.parameters()), lr=optim['lr'], alpha=0.9)
     reg.set_optimizer(reg_optimizer)
 
     learner.add_reguralizer('d', reg, alpha)
     # log
-    writer = SummaryWriter()
+    if tbx :
+        writer = SummaryWriter()
 
     print("Optimization")
     EVALUATE_PER = optim['num_batch'] / 20
     start_time = time.time()
 
-    learner.set_loader(train_source_dataset, train_target_dataset, optim['batch_size'])
+    learner.set_loader(train_dataset, source_sampler, optim['batch_size'])
     for batch_idx in range(optim['num_batch']):
         learner.update_reguralizers()
         optimizer.zero_grad()
@@ -91,7 +97,7 @@ if __name__ == '__main__':
         valid_result = learner.evaluate(valid_loader, None, True)
         test_result = learner.evaluate(test_loader, None, False)
         external_result = da_check_invariance(
-            learner.E, train_source_dataset, train_target_dataset, 1000, lr=0.001, hiddens=[E.output_shape()[1]], verbose=0)
+            learner.E, train_dataset, 1000, lr=0.001, hiddens=[E.output_shape()[1]], verbose=0)
         d_log = "valid domain loss: %.4f || external domain acc: %.4f " % (valid_result['d-loss'], external_result['valid-domain-accuracy'])
         # HDivergence
 
@@ -101,10 +107,11 @@ if __name__ == '__main__':
         base_log = "%s [%06d, %d s (%d s, %d s)] || valid acc: %.3f, valid loss: %.4f|| test acc: %.3f, test loss: %.4f " % (
             "DAN", batch_idx+1, int(elapsed_time2), int(elapse_train_time), int(elapsed_time1), valid_result['y-accuracy'], valid_result['y-loss'], test_result['y-accuracy'], test_result['y-loss']) 
         print(base_log + ' || ' + d_log)
-        writer.add_scalar('c_loss/valid/%s' % source_domain[0], valid_result['y-loss'], batch_idx)
-        writer.add_scalar('c_acc/valid/%s' % source_domain[0], valid_result['y-accuracy'], batch_idx)
-        writer.add_scalar('c_loss/test/%s' % target_domain[0], test_result['y-loss'], batch_idx)
-        writer.add_scalar('c_acc/test/%s' % target_domain[0], test_result['y-accuracy'], batch_idx)
-        writer.add_scalar('check_invariance/acc', external_result['valid-domain-accuracy'], batch_idx)
+        if tbx :
+            writer.add_scalar('c_loss/valid/%s' % source_domain[0], valid_result['y-loss'], batch_idx)
+            writer.add_scalar('c_acc/valid/%s' % source_domain[0], valid_result['y-accuracy'], batch_idx)
+            writer.add_scalar('c_loss/test/%s' % target_domain[0], test_result['y-loss'], batch_idx)
+            writer.add_scalar('c_acc/test/%s' % target_domain[0], test_result['y-accuracy'], batch_idx)
+            writer.add_scalar('check_invariance/acc', external_result['valid-domain-accuracy'], batch_idx)
 
         start_time = time.time()
--- pytransfer/reguralizer/dan.py
+++ pytransfer/reguralizer/dan.py
@@ -2,7 +2,7 @@
 from torch import nn
 
 from pytransfer.reguralizer.utils import Discriminator
-from pytransfer.reguralizer import _Reguralizer, _DAReguralizer
+from pytransfer.reguralizer import _Reguralizer
 
 
 class DANReguralizer(_Reguralizer):
@@ -67,65 +67,6 @@ class DANReguralizer(_Reguralizer):
             self.optimizer.zero_grad()
             X, _, d = self.get_batch()
             d_loss = self.d_loss(X, _, d)
-            d_loss.backward()
-            self.optimizer.step()
-
-    def parameters(self):
-        return self.D.parameters()
-
-class DADANReguralizer(_DAReguralizer):
-    def __init__(self, learner, D=None, discriminator_config=None, K=1):
-        """
-        Initialize dan base reguralizer
-
-        Parameter
-        ---------
-        learner : instance of Learner
-          TBA
-        discriminator_config : dict
-          configuration file for the discriminator
-        K : int
-          the # update of D in each iterations
-        """
-        super(DADANReguralizer, self).__init__()
-
-        self.stop_update = D is not None  # if D is shared with others, then not update here
-        if D is None:
-            D = Discriminator(**discriminator_config)
-        self.D = D.cuda()
-        print(self.D)
-        self.num_output = self.D.num_domains
-        # TODO: DANReguralizer should not assume that D has an attribute num_domain
-
-        self.learner = learner
-        self.K = K
-        self.criterion = nn.NLLLoss()
-        self.loader = None
-
-
-    def loss(self, X_s, y_s, X, d):
-        d_pred = self(X)
-        d_loss = self.criterion(d_pred, d)
-        return -1 * d_loss
-
-    def forward(self, X):
-        z = self.learner.E(X)
-        return self.D(z)
-
-    def d_loss(self, X_s, y_s, X, d):
-        z = self.learner.E(X)
-        d_pred = self.D(z)
-        d_loss = self.criterion(d_pred, d)
-        return d_loss
-
-    def update(self):
-        if self.stop_update:
-            return None
-
-        for _ in range(self.K):
-            self.optimizer.zero_grad()
-            _, _, X, d = self.get_batch()
-            d_loss = self.d_loss(_, _, X, d)
             d_loss.backward()
             self.optimizer.step()
 
