b755eb7c66e9bab0710fad9dd423a893fe5f9ab6
Yusuke IWASAWA <iwasawa@weblab.t.u-tokyo.ac.jp>
2020-02-17 16:04:07 +0900

    Refactor: dependency management

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 from setuptools import setup, find_packages
 
 # currently torch==0.3 is supported
-requires = ['numpy',
-            'tensorflow==1.15.2',
-            'memoize==1.0.0',
-            'luigi==2.6.2',
-            'wget==3.2',
-            'torch==0.3.*',
-            'torchvision==0.2.1',
-            'tqdm==4.23.2',
-            'sacred==0.7.4',
-            'gitpython==2.1.10',
-            'tensorboardX==1.2',
-            'rarfile',
-            'pandas',
-            'matplotlib',
-            'seaborn',
-            'scikit-learn']
+requires = []
 
 setup(
     name='pytransfer',
