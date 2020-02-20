099fac7ce332817661a8fa93f9a209eb2c5deabe
Yusuke IWASAWA <iwasawa@weblab.t.u-tokyo.ac.jp>
2020-02-20 19:38:41 +0900

    refactor: use pytorch lightning

--- examples/dg_dan_mnistr.py
+++ examples/dg_dan_mnistr.py
@@ -2,24 +2,34 @@
 
 """ Example for domain generalization using domain adversarial training.
 
-
-Reference: TODO
-
 """
+from future.utils import iteritems
+import os
+from collections import OrderedDict
+
+import numpy as np
+from sklearn import metrics
+import torch
+from torch import nn
+from torch.utils import data
+
 import six
 import time
 import random
 import torch
 from torch.optim import RMSprop
 import torch.utils.data as data
+from exp_utils import check_invariance
+import logging
+
+import pytorch_lightning as pl
+from pytorch_lightning.logging import TensorBoardLogger
+
 from pytransfer.datasets.base import Subset
 from pytransfer.datasets import MNISTR
 from pytransfer.trainer import Learner
 from mnistr_network import Encoder, Classifier
 from pytransfer.regularizer.dan import DANReguralizer
-from exp_utils import check_invariance
-
-import pytorch_lightning as pl
 
 def domain_wise_splits(dataset, split_size, random_seed=1234):
     datasets1 = []
@@ -44,8 +54,6 @@ def domain_wise_splits(dataset, split_size, random_seed=1234):
     return datasets1, datasets2
 
 
-# class
-
 def prepare_datasets(train_domain, test_domain):
     train_valid_dataset = MNISTR(train_domain)
     test_dataset = MNISTR(test_domain)
@@ -54,21 +62,173 @@ def prepare_datasets(train_domain, test_domain):
     return train_dataset, valid_dataset, test_dataset
 
 
+class DomainGeneralization(pl.LightningModule):
+    def __init__(self, E, M, reguralizers, reguralizers_weight, optim, datasets):
+        super(DomainGeneralization, self).__init__()
+        self.E = E
+        self.M = M
+        self.regularizers = OrderedDict()
+        for reg, weight in zip (reguralizers, reguralizers_weight):
+            self.add_regularizer(reg.__class__.__name__, reg, weight)
+        self.criterion = nn.NLLLoss()
+        self.optim = optim
+        self.datasets_config = datasets
+        
+        self.datasets = prepare_datasets(datasets['train_domain'], datasets['test_domain'])
+        logging.info("Initialized")
+
+    def add_regularizer(self, name, regularizer, alpha):
+        assert name not in self.regularizers, "name {} is already registered".format(name)
+        self.regularizers[name] = (regularizer, alpha)
+
+    def configure_optimizers(self):
+        logging.info("Configure optimizers")
+        optim = self.optim
+        opt_list = []
+        opt_list.append(RMSprop(list(self.E.parameters())+list(self.M.parameters()), lr=optim['lr'], alpha=0.9))
+
+        for reg, _ in self.regularizers.values():
+            reg.set_optimizer(RMSprop(filter(lambda p: p.requires_grad, reg.parameters()), lr=optim['lr'], alpha=0.9))
+            reg.set_loader(self.datasets[0], self.optim['batch_size'])
+            # opt_list.append(RMSprop(filter(lambda p: p.requires_grad, reg.parameters()), lr=optim['lr'], alpha=0.9))
+        return opt_list, []
+
+    @pl.data_loader
+    def train_dataloader(self):
+        dataset = self.datasets[0]
+        return data.DataLoader(dataset, batch_size=self.optim['batch_size'], shuffle=True)
+
+    def forward(self, X):
+        z = self.E(X)
+        y = self.M(z)
+        return y
+
+    def training_step(self, batch, batch_idx):
+        self.update_regularizers()
+        X, y, d = batch
+        loss = self.loss(X.float(), y.long(), d.long())
+        return {'loss': loss, 'log': {'train_loss': loss}}
+
+    def update_regularizers(self):
+        for regularizer, _ in self.regularizers.values():
+            regularizer.update()
+
+    def loss(self, X, y, d):
+        yhat = self(X)
+        y_loss = self.criterion(yhat, y)
+        loss = y_loss
+        for regularizer, alpha in self.regularizers.values():
+            loss += alpha * regularizer.loss(self.E(X), y, d)
+        return loss
+
+    def losses(self, X, y, d):
+        yhat = self(X)
+        y_loss = self.criterion(yhat, y)
+        losses = {}
+        losses['y'] = y_loss.item()
+        for i, (regularizer, alpha) in enumerate(self.regularizers.values()):
+            losses[i] = regularizer.loss(self.E(X), y, d).item()
+        return losses
+
+    def set_loader(self, dataset, batch_size):
+        self.loader = data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=True)
+        for regularizer, _ in self.regularizers.values():
+            if regularizer.loader is None:
+                regularizer.set_loader(dataset, batch_size)
+        return self.loader
+
+    def get_batch(self, as_variable=True, device='cpu'):
+        assert self.loader is not None, "Please set loader before call this function"
+        X, y, d = self.loader.__iter__().__next__()
+        if as_variable:
+            X = X.float().to(device)
+            y = y.long().to(device)
+            d = d.long().to(device)
+        return X, y, d
+
+    def predict_y(self, input_data):
+        return self.M(self.E(input_data))
+
+    def evaluate(self, loader, nb_batch=None):
+        """
+        Evaluate model given data loader
+
+        Parameter
+        ---------
+        nb_batch : int (default: None)
+          # batch to calculate the loss and accuracy
+        loader : DataLoader
+          data loader
+
+        """
+        if nb_batch is None:
+            nb_batch = len(loader)
+        self.eval()
+        result = OrderedDict()
+
+        # evaluate main
+        for k, v in iteritems(self._evaluate(loader, nb_batch)):
+            result['{}-{}'.format('y', k)] = v
+
+        # evaluate regularizer
+        for name, (regularizer, _) in iteritems(self.regularizers):
+            for k, v in iteritems(regularizer._evaluate(loader, nb_batch)):
+                result['{}-{}'.format(name, k)] = v
+
+        self.train()
+        return result
+
+    def _evaluate(self, loader, nb_batch):
+        ys = []
+        pred_ys = []
+        loss = 0
+        criterion = nn.NLLLoss()
+        with torch.no_grad():
+            for i, (X, y, d) in enumerate(loader):
+                X = X.float()
+                target = y.long()
+                pred_y = self.predict_y(X)
+                loss += criterion(pred_y, target).item()
+                pred_y = np.argmax(pred_y.data, axis=1)
+                ys.append(y.numpy())
+                pred_ys.append(pred_y.numpy())
+                if i+1 == nb_batch:
+                    break
+            loss /= nb_batch
+        # print(nb_batch, loss)
+
+        y = np.concatenate(ys)
+        pred_y = np.concatenate(pred_ys)
+
+        result = OrderedDict()
+        result['accuracy'] = metrics.accuracy_score(y, pred_y)
+        result['f1macro'] = metrics.f1_score(y, pred_y, average='macro')
+        result['loss'] = loss
+        return result
+
+    def save(self, out, prefix=None):
+        names = ['E.pth', 'M.pth']
+        if prefix is not None:
+            names = [prefix + '-' + x for x in names]
+        names = [os.path.join(out, x) for x in names]
+
+        for net, name in zip([self.E, self.M], names):
+            torch.save(net.state_dict(), name)
+        return names        
+
+
+
 if __name__ == '__main__':
     print("Execute example")
     # Parameters
-    train_domain = ['M0', 'M15', 'M30', 'M45', 'M60']
-    test_domain = ['M75']
-    optim = {'lr': 0.001, 'batch_size': 128, 'num_batch': 5000}
+    datasets = {'name': "MNISTR", 'train_domain': ['M0', 'M15', 'M30', 'M45', 'M60'], 'test_domain': ['M75']}
+    optim = {'lr': 0.001, 'batch_size': 128, 'num_batch': 100}
     alpha = 1.0
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
     print("Load datasets")
-    train_dataset, valid_dataset, test_dataset = prepare_datasets(train_domain, test_domain)
-    print(len(train_dataset), len(valid_dataset), len(test_dataset))
-    valid_loader = data.DataLoader(valid_dataset, batch_size=optim['batch_size'], shuffle=True)
-    test_loader = data.DataLoader(test_dataset, batch_size=optim['batch_size'], shuffle=True)
 
     print("Build model...")
     E = Encoder(MNISTR.get('input_shape'), hidden_size=400)
@@ -77,51 +237,14 @@ if __name__ == '__main__':
 
     print(E)
     print(M)
-    learner = Learner(E, M).to(device)
-
-    optimizer = RMSprop(learner.parameters(), lr=optim['lr'], alpha=0.9)
-    print(optimizer)
-
     print("Set regularizer")
     discriminator_config = {
-        "num_domains": len(train_dataset.datasets),
+        "num_domains": len(datasets['train_domain']),
         "input_shape": E.output_shape(), 'hiddens': [100]}
+    reg = DANReguralizer(feature_extractor=E, discriminator_config=discriminator_config)
 
-    reg = DANReguralizer(feature_extractor=learner.E, discriminator_config=discriminator_config).to(device)
-    reg_optimizer = RMSprop(filter(lambda p: p.requires_grad, reg.parameters()), lr=optim['lr'], alpha=0.9)
-    reg.set_optimizer(reg_optimizer)
+    model = DomainGeneralization(E, M, [reg], [alpha], optim, datasets)
+    logger = TensorBoardLogger("tb_logs", name="my_model")
+    trainer = Trainer(logger=logger)
 
-    learner.add_regularizer('d', reg, alpha)
-
-    print("Optimization")
-    EVALUATE_PER = optim['num_batch'] / 20
-    start_time = time.time()
-
-    learner.set_loader(train_dataset, optim['batch_size'])
-    for batch_idx in range(optim['num_batch']):
-        learner.update_regularizers()
-        optimizer.zero_grad()
-        X, y, d = learner.get_batch()
-        loss = learner.loss(X, y, d)
-        loss.backward()
-        optimizer.step()
-        learner.losses(X, y, d)
-        if (batch_idx+1) % EVALUATE_PER != 0:
-            continue
-        # train_result = evaluate(learner, train_loader, 100)
-        elapse_train_time = time.time() - start_time
-        valid_result = learner.evaluate(valid_loader, None)
-        test_result = learner.evaluate(test_loader, None)
-        external_result = check_invariance(
-            learner.E, train_dataset, 1000, valid_dataset=valid_dataset, lr=0.001, hiddens=[800], verbose=0)
-        d_log = "domain d: %.4f || external: %.4f " % (valid_result['d-loss'], external_result['valid-domain-accuracy'])
-
-        # HDivergence
-        elapsed_time1 = time.time() - start_time
-        elapsed_time2 = time.time() - start_time
-
-        base_log = "%s [%06d, %d s (%d s, %d s)] || valid acc: %.3f, valid loss: %.4f|| test acc: %.3f, test loss: %.4f " % (
-            "DAN", batch_idx+1, int(elapsed_time2), int(elapse_train_time), int(elapsed_time1), valid_result['y-accuracy'], valid_result['y-loss'], test_result['y-accuracy'], test_result['y-loss']) 
-        print(base_log + ' || ' + d_log)
-
-        start_time = time.time()
+    trainer.fit(model)
